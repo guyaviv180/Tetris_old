8f5b01e64578d658906b3e3046ac60689897eea0
guy aviv <guyaviv180@gmail.com>
2021-09-20 14:24:23 +0300

    Add project files.

---
+++ Tetris/Game.js
@@ -0,0 +1,70 @@
+﻿var rightArrow = false;
+var leftArrow = false;
+var upArrow = false;
+var downArrow = false;
+var length = 25;
+var arr = [new I, new T];
+var current = 0;
+var stop = false;
+
+window.onload = function () {
+    canvas = document.getElementById("myCanvas");
+    context = canvas.getContext("2d");
+    setInterval(function () {
+        draw();
+        dropPiece(arr[current]);
+    }, 500)
+    addEventListener("keydown", onKeyDown);
+}
+
+function getPiece(arr) {
+    piece = arr[current + 1];
+}
+
+function dropPiece(p) {
+    if (piece.Block[3].y < (500 - length)) {
+
+    }
+    for (var i = 0; i < 4; i++) {
+        p.Block[i].y += length;
+    }
+}
+
+function draw() {
+    drawBoard();
+    drawPiece()
+}
+
+function drawPiece() {
+    for (var i = 0; i < 4; i++) {
+        drawRectangle(piece.Block[i].x, piece.Block[i].y, length, length, piece.color);
+    }
+}
+
+function drawBoard() {
+    drawRectangle(0, 0, 250, 500, "white", 0, "");
+    for (var i = 1; i < 10; i++) {
+        drawLine((length * i), 0, (length * i), (length * 20), 1, "grey");
+    }
+    for (var i = 1; i < 20; i++) {
+        drawLine(0, (length * i), (length * 10), (length * i), 1, "grey");
+    }
+}
+
+function onKeyDown(event) {
+    var keyCode = event.keyCode;
+    switch (keyCode) {
+        case 39:
+            rightArrow = true;
+            break;
+        case 37:
+            leftArrow = true;
+            break;
+        case 38:
+            upArrow = true;
+            break;
+        case 40:
+            downArrow = true;
+            break;
+    }
+}
---
+++ Tetris/Piece.js
@@ -0,0 +1,42 @@
+﻿class Block {
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+        this.length = 25;
+    }
+}
+class Piece {
+    constructor(color) {
+        this.Block = new Block(4)
+        this.color = color;
+    }
+
+    isTouching(piece) {
+        for (var i = 0; i < 4; i++) {
+            for (var j = 0; j < 4; j++) {
+                if (this.Block[i].y + length == piece.Block[j].y && this.Block[i].x == piece.Block[j].x) {
+                    return true
+                }
+            }
+        }
+    }
+}
+
+class I extends Piece {
+    constructor() {
+        super("cyan");
+        this.Block[0] = new Block(length * 3, length * 0)
+        this.Block[1] = new Block(length * 4, length * 0)
+        this.Block[2] = new Block(length * 5, length * 0)
+        this.Block[3] = new Block(length * 6, length * 0)
+    }
+}
+class T extends Piece {
+    constructor() {
+        super("purple");
+        this.Block[0] = new Block(length * 4, length * 0)
+        this.Block[1] = new Block(length * 3, length * 1)
+        this.Block[2] = new Block(length * 4, length * 1)
+        this.Block[3] = new Block(length * 5, length * 1)
+    }
+}
